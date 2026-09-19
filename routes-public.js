3360ed81c39dd6563979c1f3b29db9ceabb903d8
nishuyadav45111976-gif <nishuyadav45111976@gmail.com>
2026-09-19 16:16:50 +0530

    Add files via upload

--- routes-public.js
+++ routes-public.js
@@ -399,7 +399,7 @@ router.get('/history', async (req, res) => {
       const match = allResults.find((r) => r.lotteryId === lottery.id && r.date === date);
       return match ? match.resultText : 'XX';
     });
-    return { date, cells };
+    return { date, dateLabel: shortDateLabel(date), cells };
   });
 
   res.render('history', {
